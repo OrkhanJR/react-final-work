df541784948000e546f1775ad3c326d522be1975
Orkhan <mammedov.01@mail.ru>
2024-02-29 20:58:00 +0400

    Error page and routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import MainPage from "./components/MainPage/MainPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <MainPage />,
+  errorElement: <ErrorPage />
+}]);
 
 const App = () => {
-  return (
-    <>
-      <MainPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+import { Provider } from "react-redux";
+import store from "./store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootContainer = document.querySelector("#root");
 
-root.render(
+if (rootContainer === null) throw new Error("Can't find root container");
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
